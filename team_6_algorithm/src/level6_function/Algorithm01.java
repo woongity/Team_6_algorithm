694fa7f1b88d8b7401caa59816c3d14f171c4e4b
임예나(Lim Yena) <dpsk331@naver.com>
2021-08-19 19:14:33 +0900

    Update Algorithm01.java

--- team_6_algorithm/src/level6_function/Algorithm01.java
+++ team_6_algorithm/src/level6_function/Algorithm01.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 // 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성
 // a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
 // 리턴값: a에 포함되어 있는 정수 n개의 합
-public class algorithm01 {
+public class Algorithm01 {
 	long sum(int[] a) {
 		int n = a.length;
 		long ans = 0;
