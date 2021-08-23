4ce6aac7fec7ee085909c010230745345dc87825
eunsol <a2670624@naver.com>
2021-08-24 01:11:15 +0900

    8/24 string

---
+++ stage07/string/boj2941.java
@@ -0,0 +1,20 @@
+package stage07.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        String[] kro = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        for(int i=0;i<kro.length;i++) {
+            input = input.replace(kro[i],"a");
+        }
+
+        System.out.println(input.length());
+    }
+}
