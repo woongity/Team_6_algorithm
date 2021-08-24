93048fce5c7708683fd381860eec567215fdd25c
blossom4 <cho8213456@gmail.com>
2021-08-25 00:52:20 +0900

    Oracle DataBase Oracle 210825

--- SQL_exercise/종합문제.sql
+++ SQL_exercise/종합문제.sql
@@ -142,16 +142,27 @@ where e.deptno = d.deptno and d.deptno = 30;
 
 --30> 모든 사원의 급여 최고액,최저액,총액 및 평균액을 출력하되 
 -- 각 컬럼명을 Maximum,Minimum,Sum,Average로 지정하여 출력하라.
+select max(sal) as Maximum, min(sal) as Minimum, sum(sal) as Sum, trunc(avg(sal)) as Average
+from emp;
 
 --31> 각 직업별로 급여 최저액.최고액,총액 및 평균액을 출력하라.
+select  min(sal), max(sal), sum(sal), trunc(avg(sal))
+from emp
+group by job;
 
 --32> 직업이 동일한 사람 수를 직업과 같이 출력하라.
+select count(empno), job
+from emp
+group by job;
 
 --33> 관리자의 수를 출력하되, 관리자 번호가 중복되지 않게하라.
 -- 그리고, 컬럼명을 Number of Manager로 지정하여 출력하라.
-
+select distinct count(mgr) as "Number of Manager"
+from emp;
 
 --34> 최고 급여와 최저 급여의 차액을 출력하라.
+select max(sal) - min(sal)
+from emp;
 
 --35> 관리자 번호 및 해당 관리자에 속한 사원들의 최저 급여를 출력하라.
 -- 단, 관리자가 없는 사원 및 최저 급여가 1000 미만인 그룹은 제외시키고 
