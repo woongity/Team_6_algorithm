88740e4d8379a04894521fc0ad8b75a8622d759d
blossom4 <cho8213456@gmail.com>
2021-08-26 22:33:56 +0900

    SQL Exercise 36 ~ 40 210826

--- SQL_exercise/종합문제.sql
+++ SQL_exercise/종합문제.sql
@@ -167,26 +167,50 @@ from emp;
 --35> 관리자 번호 및 해당 관리자에 속한 사원들의 최저 급여를 출력하라.
 -- 단, 관리자가 없는 사원 및 최저 급여가 1000 미만인 그룹은 제외시키고 
 -- 급여를 기준으로 출력 결과를 내림차순으로 정렬하라.
-
+select mgr, min(sal)
+from emp
+where mgr is not null
+group by mgr
+having min(sal) >= 1000
+order by min(sal) desc;
 
 --36> 부서별로 부서이름, 부서위치, 사원 수 및 평균 급여를 출력하라.
 -- 그리고 각각의 컬럼명을 부서명,위치,사원의 수,평균급여로 표시하라.
-
+select d.dname, d.loc, count(e.empno), trunc(avg(sal))
+from emp e, dept d 
+where e.deptno = d.deptno
+group by d.deptno, d.dname, d.loc;
 
 --37> Smith와 동일한 부서에 속한 모든 사원의 이름 및 입사일을 출력하라.
 -- 단, Smith는 제외하고 출력하시오
+-- e1: SMITH
+-- e2: SMITH와 동일한 부서에 속한 사원
+select e2.ename, e2.hiredate
+from emp e1, emp e2
+where e1.ename = 'SMITH' and e1.deptno = e2.deptno and e2.ename != 'SMITH';
 
 
 --38> 자신의 급여가 평균 급여보다 많은 모든 사원의 사원 번호, 이름, 급여를 
 --    표시하는 질의를 작성하고 급여를 기준으로 결과를 내림차순으로 정렬하라.
-
+select empno, ename, sal
+from emp
+where sal > (select avg(sal) from emp)
+order by sal desc;
 
 --39> 이름에 T가 들어가는 사원의 속한 부서에서 근무하는 모든 사원의 사원번호
 -- 및 이름을 출력하라.
-
+-- e1: 이름에 T가 들어가는 사원
+-- e2: e1과 같은 부서에서 근무하는 사원
+select distinct(e2.empno), e2.ename
+from emp e1, emp e2
+where e1.ename like '%T%' and e1.deptno = e2.deptno;
 
 --40> 부서위치가 Dallas인 모든 사원의 이름,부서번호 및 직위를 출력하라.
-
+-- e: emp table
+-- d: dept table
+select e.ename, e.empno, e.job
+from emp e, dept d
+where e.deptno = d.deptno and d.loc = 'DALLAS';
 
 --41> KING에게 보고하는 모든 사원의 이름과 급여를 출력하라.
 
