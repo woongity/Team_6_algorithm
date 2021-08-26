0127cb34f0b3957a2df4181c065638575ecae9a1
blossom4 <cho8213456@gmail.com>
2021-08-26 22:40:04 +0900

    10870 modified

--- Baekjoon/q10870_피보나치수5/Main.java
+++ Baekjoon/q10870_피보나치수5/Main.java
@@ -7,8 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 	
 	public static int fibonacci(int n) {
-		if (n == 0) return 0;
-		else if (n == 1) return 1;
+		if (n <= 1) return n;
 		
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
