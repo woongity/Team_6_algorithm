e0dae7a7c9065466b2fe6d4e3b9d9c87963917a8
임예나(Lim Yena) <dpsk331@naver.com>
2021-08-29 23:51:27 +0900

    Baekjoon2447 별 찍기 - 10
    문제 해결에 대한 방법 연구 필요,, 스터디 후 다시 풀어보기

---
+++ team_6_algorithm/src/level10_recursion/Baekjoon2447.java
@@ -0,0 +1,24 @@
+package level10_recursion;
+
+import java.util.Scanner;
+
+// 백준 2447 문제] 별 찍기 - 10
+public class Baekjoon2447 {
+    public static void star() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ex = n/3;
+
+        char[][] star = new char[n][n];
+
+        // (1,1) 일 때 공백
+
+
+
+
+    }
+}
