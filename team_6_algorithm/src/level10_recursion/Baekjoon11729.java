a577b912c267aa332c7d7e54c1189640cfcf101f
임예나(Lim Yena) <dpsk331@naver.com>
2021-08-29 23:50:28 +0900

    Baekjoon11729 하노이 탑 이동 순서
    문제에 대한 이해가 더 필요(재귀, 하노이 탑)

---
+++ team_6_algorithm/src/level10_recursion/Baekjoon11729.java
@@ -0,0 +1,38 @@
+package level10_recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 11729 문제] 하노이 탑 이동 순서
+public class Baekjoon11729 {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+
+        Hanoi(N, 1, 2, 3);
+        System.out.println(sb);
+    }  //main end
+
+    public static void Hanoi(int N, int A, int B, int C) {
+        // 이동할 원반의 수가 1개라면?
+        if (N == 1) {
+            sb.append(A + " " + C + "\n");
+            return;
+        }
+        // STEP 1 : N-1개를 A에서 B로 이동
+        Hanoi(N - 1, A, C, B);
+
+        // STEP 2 : 1개를 A에서 C로 이동
+        sb.append(A + " " + C + "\n");
+
+        // STEP 3 : N-1개를 B에서 C로 이동
+        Hanoi(N - 1, B, A, C);
+
+    } //Hanoi end
+}
