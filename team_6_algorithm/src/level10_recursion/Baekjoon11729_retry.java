0461880c9eeb0b9248e05b978540a4cc9a7b38e8
임예나(Lim Yena) <dpsk331@naver.com>
2021-08-31 16:30:30 +0900

    Baekjoon11729 하노이 탑 이동 순서 - 재도전

---
+++ team_6_algorithm/src/level10_recursion/Baekjoon11729_retry.java
@@ -0,0 +1,35 @@
+package level10_recursion;
+
+import java.util.Scanner;
+
+// 백준 11729 문제] 하노이 탑 이동 순서
+public class Baekjoon11729_retry {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 원판의 개수
+
+        sb.append((int)(Math.pow(2,n)-1)).append('\n');
+
+        hanoi(n, 1,2, 3);
+        System.out.println(sb);
+    }
+
+    public static void hanoi(int n, int from, int via, int to) {
+        // Base Condition: 원반 1개일 경우
+        if(n == 1) {
+            sb.append(from + " " + to + "\n");
+            return;
+        }
+
+        // 원반 n-1개  1 -> 2 기둥으로 옮긴다.
+        hanoi(n-1, from, to, via);
+
+        // 1개의 원반을 1 -> 3 기둥으로 옮긴다.
+        sb.append(from + " " + to + "\n");
+
+        // 원반 n-1개를 2 -> 3 기둥으로 옮긴다.
+        hanoi(n-1, via, from, to);
+    }
+}
